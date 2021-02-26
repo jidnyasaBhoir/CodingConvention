1386565db60eab19a6358dea0bae837fb0f5a79d
jidnyasaBhoir <jidnyasabhoir50@gmail.com>
2021-02-26 22:34:09 +0530

    Done

---
+++ EmployeeOne.java
@@ -0,0 +1,30 @@
+public class Employee
+{
+    int id;
+    double salary=0.0;
+
+    public Employee(int id,double salary) {
+        this.id = id;
+        this.salary=salary;
+    }
+
+    public Employee() {
+    }
+
+    double calTax(){
+           if(this.salary > 500000.00 && this.salary < 1000000)
+               return this.salary*0.1;
+
+           else if(this.salary >1000000.00 && this.salary < 1500000)
+               return this.salary*0.2;
+
+           else
+               return this.salary*0.3;
+        
+        for(int index=0;index<10;index++){
+
+        }
+
+    }
+
+}
